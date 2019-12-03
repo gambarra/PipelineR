cbe41eab86e4c3c073a6ccf0ec869d38c3711adb
tgambarra <thiago.gambarra@gmail.com>
2019-12-03 16:26:42 -0300

    :art: implementa result object

--- src/PipelineR/ErrorResult.cs
+++ src/PipelineR/ErrorResult.cs
@@ -5,11 +5,12 @@ namespace PipelineR
 {
     public class ErrorResult
     {
-        public ErrorResult(string source, string message,Exception exception)
+        public ErrorResult(string source, string message,Exception exception, object result=null)
         {
             this.Exception = exception;
             this.Source = source;
             this.Message = message;
+            this.Result = result;
         }
         public ErrorResult(string source, Exception exception):this(source,null,exception) {
 
@@ -23,6 +24,14 @@ namespace PipelineR
         public ErrorResult( Exception exception):this(null, null, exception) {
 
         }
+        public ErrorResult(string source, object result) : this(source, null,null,result)
+        {
+
+        }
+        public ErrorResult( object result) : this(null, null, null, result)
+        {
+
+        }
 
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Source { get; private set; }
@@ -30,6 +39,8 @@ namespace PipelineR
         public string Message { get; private set; }
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public Exception Exception { get; private set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public object Result { get; set; }
 
     }
 }
